0536a2cc2f95e92236ccce7d17d3fbf3279c3e2b
Abdelhamid CHIKH AISSA <Abdelhamidchikhaissa@gmail.com>
2022-09-18 23:54:33 +0100

    creat the variables

--- Lesson 3 assignment/main.js
+++ Lesson 3 assignment/main.js
@@ -1,3 +1,7 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext('2d');
 
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 3;
+ctx.strokeStyle = '#a2f100';
