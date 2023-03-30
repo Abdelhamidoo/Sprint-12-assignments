6eafb45847683b4f23aac393cf159d388260aa26
Abdelhamid CHIKH AISSA <Abdelhamidchikhaissa@gmail.com>
2023-03-30 17:54:16 +0100

    finish the second part

--- Final project/main.js
+++ Final project/main.js
@@ -1,12 +1,24 @@
 "use strict";
 
+$(document).ready(() => {
+    $('#button').click(() => {
+        let RGBvalues = randomRGBcolor();
+        let rgbcolor = "rgb(" + RGBvalues[0] + "," + RGBvalues[1] + "," + RGBvalues[2] + ")";
+        $('body').css('background-color', rgbcolor);
+        $('#color').text(rgbcolor);
+    })
+});
+
 function randomNumber(){
     return Math.floor(Math.random() * 256);
 };
 
+function randomRGBcolor(){
+    let red = randomNumber();
+    let green = randomNumber();
+    let blue = randomNumber();
+    return [red, green, blue];
+};
 
-$(document).ready(() => {
-    $('#button').click(() => {
 
-    })
-});
+
